5c302b4d0e4b320a098bf47a13cc7e0b1afb176a
Oleg Sadchikov <71182254+KiditsGood@users.noreply.github.com>
2022-10-31 11:35:34 +0300

    finish MAYBE

--- js/history.js
+++ js/history.js
@@ -1,3 +1,19 @@
 $('.block__tabs-item').click(function () {
     $(this).addClass('active').siblings().removeClass('active')
 })
+
+$('.visible').click(function () {
+    $(this.lastElementChild).slideToggle(200)
+})
+
+$('.block__date').click(function () {
+    $('.date__hidden').slideToggle(200)
+
+})
+
+$('.date__hidden .block__date-text').click(function () {
+    $(this).addClass('active').siblings().removeClass('active')
+
+    $('.date__hidden').slideUp(200)
+
+})
